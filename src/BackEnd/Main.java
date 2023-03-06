d602db6d181d9d4e0497bca567ca39f2a059c746
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-03-06 12:44:58 -0500

    Complete the BackEnd for the program

--- src/BackEnd/Main.java
+++ src/BackEnd/Main.java
@@ -18,20 +18,56 @@ public class Main {
         File file = path.toFile();
         File outputFile = new File("output.txt");
         PrintWriter out = new PrintWriter(outputFile);
+        boolean checkedFirstLineNonZero = false;
 
         Scanner in = new Scanner(file);
+
+        LinkedHashSet<String> trueAtomsFromInput = new LinkedHashSet<>();
 
         while (in.hasNextLine()) {
             String line = in.nextLine();
             // Handle the easy case, where the first line is a 0 (i.e. no solution)
             if (line.equals("0")) {
-                System.out.println("NO SOLUTION");
-                out.println("NO SOLUTION");
-                out.close();
-                return;
-            }   
+                if (!checkedFirstLineNonZero) {
+                    System.out.println("NO SOLUTION");
+                    out.println("NO SOLUTION");
+                    out.close();
+                    return;
+                }
+                break;
+            }
+            checkedFirstLineNonZero = true;
+
+            String[] inputLine = line.split(" ");
+            String atom = inputLine[0];
+            boolean isTrue = inputLine[1].equals("T");
+            if (isTrue) {
+                trueAtomsFromInput.add(atom);
+            }
         }
 
+        // Now we get the corresponding Stringified atoms from the input
+        HashMap<String, String> stringifiedAtomsFromInput = new HashMap<>();
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            String[] inputLine = line.split(" ");
+            String atomNumber = inputLine[0];
+            String stringifiedAtom = inputLine[1];
+            stringifiedAtomsFromInput.put(atomNumber, stringifiedAtom);
+        }
+
+        // Now we have the true atoms from the input, and the corresponding stringified atoms from the input.
+        // Only print the stringified atoms that are true and are "At" atoms, not "Has" atoms.
+        for (String atomNumber : trueAtomsFromInput) {
+            String stringifiedAtom = stringifiedAtomsFromInput.get(atomNumber);
+            if (stringifiedAtom.startsWith("At")) {
+                String node = stringifiedAtom.split(",")[0].substring(3);
+                System.out.print(node + " ");
+                out.print(node + " ");
+            }
+        }
+
+        out.close();
     }
 
 }
